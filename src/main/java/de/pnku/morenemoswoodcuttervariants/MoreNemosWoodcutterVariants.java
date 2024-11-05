c51ace591a06fac57da8c73cebf6ea2db7622bde
pnk2u <159317511+pnk2u@users.noreply.github.com>
2024-11-05 11:56:27 +0100

    Initial Commit (1.21.3)

--- src/main/java/de/pnku/morenemoswoodcuttervariants/MoreNemosWoodcutterVariants.java
+++ src/main/java/de/pnku/morenemoswoodcuttervariants/MoreNemosWoodcutterVariants.java
@@ -1,6 +1,7 @@
 package de.pnku.morenemoswoodcuttervariants;
 
 import de.pnku.morenemoswoodcuttervariants.block.WoodcutterVariantBlocks;
+import de.pnku.morenemoswoodcuttervariants.item.WoodcutterVariantItems;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,7 +13,8 @@ public class MoreNemosWoodcutterVariants implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+        WoodcutterVariantBlocks.registerBlocks();
+        WoodcutterVariantItems.registerItems();
 		log.info("Nemo's Woodcutters come in all* colors!");
-		WoodcutterVariantBlocks.registerBlocks();
 	}
 }
