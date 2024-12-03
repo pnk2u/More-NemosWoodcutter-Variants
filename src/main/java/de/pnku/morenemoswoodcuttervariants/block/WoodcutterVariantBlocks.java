864b26c583f06f0703e1f1ac2aad19525afe64e3
pnk2u <159317511+pnk2u@users.noreply.github.com>
2024-12-03 06:23:14 +0100

    Initial Commit (1.21.4)
     Conflicts:
    	gradle.properties

--- src/main/java/de/pnku/morenemoswoodcuttervariants/block/WoodcutterVariantBlocks.java
+++ src/main/java/de/pnku/morenemoswoodcuttervariants/block/WoodcutterVariantBlocks.java
@@ -5,6 +5,8 @@ import com.nemonotfound.nemoswoodcutter.block.WoodcutterBlock;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTabs;
@@ -17,6 +19,7 @@ import net.minecraft.world.level.material.MapColor;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 
 import static de.pnku.morenemoswoodcuttervariants.MoreNemosWoodcutterVariants.MOD_ID;
 import static de.pnku.morenemoswoodcuttervariants.MoreNemosWoodcutterVariants.log;
@@ -24,97 +27,125 @@ import static de.pnku.morenemoswoodcuttervariants.MoreNemosWoodcutterVariants.lo
 public class WoodcutterVariantBlocks {
     public static List<Block> more_woodcutters = new ArrayList<>();
 
-    public static final Block ACACIA_WOODCUTTER_BLOCK = registerBlock("acacia_woodcutter",
-            new WoodcutterBlock(BlockBehaviour.Properties.of()
-                    .sound(SoundType.WOOD)
-                    .mapColor(MapColor.COLOR_ORANGE)
-                    .instrument(NoteBlockInstrument.BASS)
-                    .strength(2.0f)));
-    public static final Block BAMBOO_WOODCUTTER_BLOCK = registerBlock("bamboo_woodcutter",
-            new WoodcutterBlock(BlockBehaviour.Properties.of()
-                    .sound(SoundType.BAMBOO_WOOD)
-                    .mapColor(MapColor.COLOR_YELLOW)
-                    .instrument(NoteBlockInstrument.BASS)
-                    .strength(2.0f)));
-    public static final Block BIRCH_WOODCUTTER_BLOCK = registerBlock("birch_woodcutter",
-            new WoodcutterBlock(BlockBehaviour.Properties.of()
-                    .sound(SoundType.WOOD)
-                    .mapColor(MapColor.SAND)
-                    .instrument(NoteBlockInstrument.BASS)
-                    .strength(2.0f)));
-    public static final Block CHERRY_WOODCUTTER_BLOCK = registerBlock("cherry_woodcutter",
-            new WoodcutterBlock(BlockBehaviour.Properties.of()
-                    .sound(SoundType.CHERRY_WOOD)
-                    .mapColor(MapColor.TERRACOTTA_WHITE)
-                    .instrument(NoteBlockInstrument.BASS)
-                    .strength(2.0f)));
-    public static final Block CRIMSON_WOODCUTTER_BLOCK = registerBlock("crimson_woodcutter",
-            new WoodcutterBlock(BlockBehaviour.Properties.of()
-                    .sound(SoundType.NETHER_WOOD)
-                    .mapColor(MapColor.CRIMSON_STEM)
-                    .instrument(NoteBlockInstrument.BASS)
-                    .strength(2.0f)));
-    public static final Block DARK_OAK_WOODCUTTER_BLOCK = registerBlock("dark_oak_woodcutter",
-            new WoodcutterBlock(BlockBehaviour.Properties.of()
-                    .sound(SoundType.WOOD)
-                    .mapColor(MapColor.COLOR_BROWN)
-                    .instrument(NoteBlockInstrument.BASS)
-                    .strength(2.0f)));
-    public static final Block JUNGLE_WOODCUTTER_BLOCK = registerBlock("jungle_woodcutter",
-            new WoodcutterBlock(BlockBehaviour.Properties.of()
-                    .sound(SoundType.WOOD)
-                    .mapColor(MapColor.DIRT)
-                    .instrument(NoteBlockInstrument.BASS)
-                    .strength(2.0f)));
-    public static final Block MANGROVE_WOODCUTTER_BLOCK = registerBlock("mangrove_woodcutter",
-            new WoodcutterBlock(BlockBehaviour.Properties.of()
-                    .sound(SoundType.WOOD)
-                    .mapColor(MapColor.COLOR_RED)
-                    .instrument(NoteBlockInstrument.BASS)
-                    .strength(2.0f)));
-    public static final Block SPRUCE_WOODCUTTER_BLOCK = registerBlock("spruce_woodcutter",
-            new WoodcutterBlock(BlockBehaviour.Properties.of()
-                    .sound(SoundType.WOOD)
-                    .mapColor(MapColor.PODZOL)
-                    .instrument(NoteBlockInstrument.BASS)
-                    .strength(2.0f)));
-    public static final Block WARPED_WOODCUTTER_BLOCK = registerBlock("warped_woodcutter",
-            new WoodcutterBlock(BlockBehaviour.Properties.of()
-                    .sound(SoundType.NETHER_WOOD)
-                    .mapColor(MapColor.WARPED_STEM)
-                    .instrument(NoteBlockInstrument.BASS)
-                    .strength(2.0f)));
+        public static final Block ACACIA_WOODCUTTER_BLOCK;
+        public static final Block BAMBOO_WOODCUTTER_BLOCK;
+        public static final Block BIRCH_WOODCUTTER_BLOCK;
+        public static final Block CHERRY_WOODCUTTER_BLOCK;
+        public static final Block CRIMSON_WOODCUTTER_BLOCK;
+        public static final Block DARK_OAK_WOODCUTTER_BLOCK;
+        public static final Block PALE_OAK_WOODCUTTER_BLOCK;
+        public static final Block JUNGLE_WOODCUTTER_BLOCK;
+        public static final Block MANGROVE_WOODCUTTER_BLOCK;
+        public static final Block SPRUCE_WOODCUTTER_BLOCK;
+        public static final Block WARPED_WOODCUTTER_BLOCK;
+
+        private static Block register(String path, Function<BlockBehaviour.Properties, Block> factory, BlockBehaviour.Properties properties) {
+            return register(keyOf(path), factory, properties);
+        }
+
+        private static ResourceKey<Block> keyOf(String path) {
+            return ResourceKey.create(Registries.BLOCK, ResourceLocation.fromNamespaceAndPath(MOD_ID, path));
+        }
+
+        public static Block register(ResourceKey<Block> key, Function<BlockBehaviour.Properties, Block> factory, BlockBehaviour.Properties properties) {
+            Block block = factory.apply(properties.setId(key));
+            return Registry.register(BuiltInRegistries.BLOCK, key, block);
+        }
+
+        static {
+            ACACIA_WOODCUTTER_BLOCK = register("acacia_woodcutter", WoodcutterBlock::new,
+                    BlockBehaviour.Properties.of()
+                            .sound(SoundType.WOOD)
+                            .mapColor(MapColor.COLOR_ORANGE)
+                            .instrument(NoteBlockInstrument.BASS)
+                            .strength(2.0f));
+            BAMBOO_WOODCUTTER_BLOCK = register("bamboo_woodcutter", WoodcutterBlock::new,
+                    BlockBehaviour.Properties.of()
+                            .sound(SoundType.BAMBOO_WOOD)
+                            .mapColor(MapColor.COLOR_YELLOW)
+                            .instrument(NoteBlockInstrument.BASS)
+                            .strength(2.0f));
+            BIRCH_WOODCUTTER_BLOCK = register("birch_woodcutter", WoodcutterBlock::new,
+                    BlockBehaviour.Properties.of()
+                            .sound(SoundType.WOOD)
+                            .mapColor(MapColor.SAND)
+                            .instrument(NoteBlockInstrument.BASS)
+                            .strength(2.0f));
+            CHERRY_WOODCUTTER_BLOCK = register("cherry_woodcutter", WoodcutterBlock::new,
+                    BlockBehaviour.Properties.of()
+                            .sound(SoundType.CHERRY_WOOD)
+                            .mapColor(MapColor.TERRACOTTA_WHITE)
+                            .instrument(NoteBlockInstrument.BASS)
+                            .strength(2.0f));
+            CRIMSON_WOODCUTTER_BLOCK = register("crimson_woodcutter", WoodcutterBlock::new,
+                    BlockBehaviour.Properties.of()
+                            .sound(SoundType.NETHER_WOOD)
+                            .mapColor(MapColor.CRIMSON_STEM)
+                            .instrument(NoteBlockInstrument.BASS)
+                            .strength(2.0f));
+            DARK_OAK_WOODCUTTER_BLOCK = register("dark_oak_woodcutter", WoodcutterBlock::new,
+                    BlockBehaviour.Properties.of()
+                            .sound(SoundType.WOOD)
+                            .mapColor(MapColor.COLOR_BROWN)
+                            .instrument(NoteBlockInstrument.BASS)
+                            .strength(2.0f));
+            PALE_OAK_WOODCUTTER_BLOCK = register("pale_oak_woodcutter", WoodcutterBlock::new,
+                    BlockBehaviour.Properties.of()
+                            .sound(SoundType.WOOD)
+                            .mapColor(MapColor.QUARTZ)
+                            .instrument(NoteBlockInstrument.BASS)
+                            .strength(2.0f));
+            JUNGLE_WOODCUTTER_BLOCK = register("jungle_woodcutter", WoodcutterBlock::new,
+                    BlockBehaviour.Properties.of()
+                            .sound(SoundType.WOOD)
+                            .mapColor(MapColor.DIRT)
+                            .instrument(NoteBlockInstrument.BASS)
+                            .strength(2.0f));
+            MANGROVE_WOODCUTTER_BLOCK = register("mangrove_woodcutter", WoodcutterBlock::new,
+                    BlockBehaviour.Properties.of()
+                            .sound(SoundType.WOOD)
+                            .mapColor(MapColor.COLOR_RED)
+                            .instrument(NoteBlockInstrument.BASS)
+                            .strength(2.0f));
+            SPRUCE_WOODCUTTER_BLOCK = register("spruce_woodcutter", WoodcutterBlock::new,
+                    BlockBehaviour.Properties.of()
+                            .sound(SoundType.WOOD)
+                            .mapColor(MapColor.PODZOL)
+                            .instrument(NoteBlockInstrument.BASS)
+                            .strength(2.0f));
+            WARPED_WOODCUTTER_BLOCK = register("warped_woodcutter", WoodcutterBlock::new,
+                    BlockBehaviour.Properties.of()
+                            .sound(SoundType.NETHER_WOOD)
+                            .mapColor(MapColor.WARPED_STEM)
+                            .instrument(NoteBlockInstrument.BASS)
+                            .strength(2.0f));
+        }
 
 
-    public static void registerBlocks() {
-        log.info("Register woodcutter variant blocks");
+        public static void registerBlocks() {
+            log.info("Register woodcutter variant blocks");
 
-        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.FUNCTIONAL_BLOCKS).register(content -> content.addAfter(ModBlocks.WOODCUTTER_BLOCK,
-                WoodcutterVariantBlocks.ACACIA_WOODCUTTER_BLOCK));
-        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.FUNCTIONAL_BLOCKS).register(content -> content.addAfter(WoodcutterVariantBlocks.ACACIA_WOODCUTTER_BLOCK,
-                WoodcutterVariantBlocks.BAMBOO_WOODCUTTER_BLOCK));
-        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.FUNCTIONAL_BLOCKS).register(content -> content.addAfter(WoodcutterVariantBlocks.BAMBOO_WOODCUTTER_BLOCK,
-                WoodcutterVariantBlocks.BIRCH_WOODCUTTER_BLOCK));
-        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.FUNCTIONAL_BLOCKS).register(content -> content.addAfter(WoodcutterVariantBlocks.BIRCH_WOODCUTTER_BLOCK,
-                WoodcutterVariantBlocks.CHERRY_WOODCUTTER_BLOCK));
-        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.FUNCTIONAL_BLOCKS).register(content -> content.addAfter(WoodcutterVariantBlocks.CHERRY_WOODCUTTER_BLOCK,
-                WoodcutterVariantBlocks.CRIMSON_WOODCUTTER_BLOCK));
-        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.FUNCTIONAL_BLOCKS).register(content -> content.addAfter(WoodcutterVariantBlocks.CRIMSON_WOODCUTTER_BLOCK,
-                WoodcutterVariantBlocks.DARK_OAK_WOODCUTTER_BLOCK));
-        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.FUNCTIONAL_BLOCKS).register(content -> content.addAfter(WoodcutterVariantBlocks.DARK_OAK_WOODCUTTER_BLOCK,
-                WoodcutterVariantBlocks.JUNGLE_WOODCUTTER_BLOCK));
-        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.FUNCTIONAL_BLOCKS).register(content -> content.addAfter(WoodcutterVariantBlocks.JUNGLE_WOODCUTTER_BLOCK,
-                WoodcutterVariantBlocks.MANGROVE_WOODCUTTER_BLOCK));
-        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.FUNCTIONAL_BLOCKS).register(content -> content.addAfter(WoodcutterVariantBlocks.MANGROVE_WOODCUTTER_BLOCK,
-                WoodcutterVariantBlocks.SPRUCE_WOODCUTTER_BLOCK));
-        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.FUNCTIONAL_BLOCKS).register(content -> content.addAfter(WoodcutterVariantBlocks.SPRUCE_WOODCUTTER_BLOCK,
-                WoodcutterVariantBlocks.WARPED_WOODCUTTER_BLOCK));
-    }
+            ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.FUNCTIONAL_BLOCKS).register(content -> content.addAfter(ModBlocks.WOODCUTTER.get(),
+                    WoodcutterVariantBlocks.ACACIA_WOODCUTTER_BLOCK));
+            ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.FUNCTIONAL_BLOCKS).register(content -> content.addAfter(ModVariantBlocks.ACACIA_WOODCUTTER_BLOCK,
+                    WoodcutterVariantBlocks.BAMBOO_WOODCUTTER_BLOCK));
+            ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.FUNCTIONAL_BLOCKS).register(content -> content.addAfter(ModVariantBlocks.BAMBOO_WOODCUTTER_BLOCK,
+                    WoodcutterVariantBlocks.BIRCH_WOODCUTTER_BLOCK));
+            ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.FUNCTIONAL_BLOCKS).register(content -> content.addAfter(ModVariantBlocks.BIRCH_WOODCUTTER_BLOCK,
+                    WoodcutterVariantBlocks.CHERRY_WOODCUTTER_BLOCK));
+            ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.FUNCTIONAL_BLOCKS).register(content -> content.addAfter(ModVariantBlocks.CHERRY_WOODCUTTER_BLOCK,
+                    WoodcutterVariantBlocks.CRIMSON_WOODCUTTER_BLOCK));
+            ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.FUNCTIONAL_BLOCKS).register(content -> content.addAfter(ModVariantBlocks.CRIMSON_WOODCUTTER_BLOCK,
+                    WoodcutterVariantBlocks.DARK_OAK_WOODCUTTER_BLOCK));
+            ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.FUNCTIONAL_BLOCKS).register(content -> content.addAfter(ModVariantBlocks.DARK_OAK_WOODCUTTER_BLOCK,
+                    WoodcutterVariantBlocks.JUNGLE_WOODCUTTER_BLOCK));
+            ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.FUNCTIONAL_BLOCKS).register(content -> content.addAfter(ModVariantBlocks.JUNGLE_WOODCUTTER_BLOCK,
+                    WoodcutterVariantBlocks.MANGROVE_WOODCUTTER_BLOCK));
+            ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.FUNCTIONAL_BLOCKS).register(content -> content.addAfter(ModVariantBlocks.MANGROVE_WOODCUTTER_BLOCK,
+                    WoodcutterVariantBlocks.SPRUCE_WOODCUTTER_BLOCK));
+            ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.FUNCTIONAL_BLOCKS).register(content -> content.addAfter(ModVariantBlocks.SPRUCE_WOODCUTTER_BLOCK,
+                    WoodcutterVariantBlocks.WARPED_WOODCUTTER_BLOCK));
+        }
 
-    private static Block registerBlock(String path, Block block) {
-        Block registeredBlock = Registry.register(BuiltInRegistries.BLOCK, ResourceLocation.tryBuild(MOD_ID, path), block);
-        Registry.register(BuiltInRegistries.ITEM, ResourceLocation.tryBuild(MOD_ID, path), new BlockItem(block, new Item.Properties()));
-        more_woodcutters.add(registeredBlock);
-        return registeredBlock;
     }
 }
