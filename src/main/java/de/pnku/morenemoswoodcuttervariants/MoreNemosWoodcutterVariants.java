d5440f9786a1fa6075afffb48f77794bb8f6a502
pnk2u <159317511+pnk2u@users.noreply.github.com>
2025-04-22 10:53:10 +0200

    Update dep and broaden nwc version compat, 1.1.5

--- src/main/java/de/pnku/morenemoswoodcuttervariants/MoreNemosWoodcutterVariants.java
+++ src/main/java/de/pnku/morenemoswoodcuttervariants/MoreNemosWoodcutterVariants.java
@@ -2,6 +2,7 @@ package de.pnku.morenemoswoodcuttervariants;
 
 import de.pnku.morenemoswoodcuttervariants.block.WoodcutterVariantBlocks;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,6 +13,13 @@ public class MoreNemosWoodcutterVariants implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		if (FabricLoader.getInstance().getAllMods().stream()
+				.noneMatch(mod -> {
+				String modId = mod.getMetadata().getId();
+				 return (modId.startsWith("nemo") && modId.endsWith("woodcutter"));
+				})) {
+			throw new RuntimeException("\"More Nemo\'s Woodcutter Variants\" requires a version of \"Nemo\'s Woodcutter\" to be installed but couldn't find any.");
+		}
 		log.info("Nemo's Woodcutters come in all* colors!");
 		WoodcutterVariantBlocks.registerBlocks();
 	}
