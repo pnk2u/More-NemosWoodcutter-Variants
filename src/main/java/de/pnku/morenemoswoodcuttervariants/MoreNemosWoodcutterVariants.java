10a9446a2891da998fbee940bdf3429cea00447c
pnku <159317511+pnk2u@users.noreply.github.com>
2025-09-06 15:51:20 +0200

    Add changelog history, simplify dependency check

--- src/main/java/de/pnku/morenemoswoodcuttervariants/MoreNemosWoodcutterVariants.java
+++ src/main/java/de/pnku/morenemoswoodcuttervariants/MoreNemosWoodcutterVariants.java
@@ -2,7 +2,6 @@ package de.pnku.morenemoswoodcuttervariants;
 
 import de.pnku.morenemoswoodcuttervariants.block.WoodcutterVariantBlocks;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.loader.api.FabricLoader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,13 +12,6 @@ public class MoreNemosWoodcutterVariants implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		if (FabricLoader.getInstance().getAllMods().stream()
-				.noneMatch(mod -> {
-				String modId = mod.getMetadata().getId();
-				 return (modId.startsWith("nemo") && modId.endsWith("woodcutter"));
-				})) {
-			throw new RuntimeException("\"More Nemo\'s Woodcutter Variants\" requires a version of \"Nemo\'s Woodcutter\" to be installed but couldn't find any.");
-		}
 		LOGGER.info("Nemo's Woodcutters come in all* colors!");
 		WoodcutterVariantBlocks.registerBlocks();
 	}
