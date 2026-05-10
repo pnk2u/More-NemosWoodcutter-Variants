e180b66efae50320d1006e7eef70d20911a3216d
pnku <pnku@pnku.de>
2026-05-10 03:28:01 +0200

    Refactor block/item registration `2.0.0-d:c2`
    - Adds MoreWoodcutterBlock and MoreWoodcutterBlocks to provide per-wood-type Woodcutter Block instances and a lookup map
    - Replaces legacy registration list with more_woodcutter_blocks
    - Adds MoreWoodcutterItems to create, register and add corresponding BlockItems to the creative tab
    - Updates client to apply cutout RenderType to blocks from more_woodcutter_blocks
    - Updates mod initializer to register blocks and items and to provide a withModId ResourceLocation helper

--- src/main/java/de/pnku/more_nemos_woodcutter_variants/MoreNemosWoodcutterVariants.java
+++ src/main/java/de/pnku/more_nemos_woodcutter_variants/MoreNemosWoodcutterVariants.java
@@ -1,7 +1,9 @@
 package de.pnku.more_nemos_woodcutter_variants;
 
-import de.pnku.more_nemos_woodcutter_variants.block.WoodcutterVariantBlocks;
+import de.pnku.more_nemos_woodcutter_variants.block.MoreWoodcutterBlocks;
+import de.pnku.more_nemos_woodcutter_variants.item.MoreWoodcutterItems;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.resources.ResourceLocation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,6 +15,11 @@ public class MoreNemosWoodcutterVariants implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		LOGGER.info("Nemo's Woodcutters come in all* colors!");
-		WoodcutterVariantBlocks.registerBlocks();
+		MoreWoodcutterBlocks.registerBlocks();
+		MoreWoodcutterItems.registerItems();
+	}
+
+	public static ResourceLocation withModId(String path) {
+		return ResourceLocation.tryBuild(MOD_ID, path);
 	}
 }
