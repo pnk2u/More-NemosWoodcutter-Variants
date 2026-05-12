26f788810d440bf2962c14201d6480670663cd37
pnku <pnku@pnku.de>
2026-05-12 04:03:02 +0200

    Add built-in datapack overwriting default WC recipe `2.0.0-d:c6`
    - Registers a built-in datapack and sets it to always enabled to override default Woodcutter recipe
    - Moves the Woodcutter recipe into that datapack and overwrites the recipe unlocking advancement
    - Adds pack.mcmeta and translation entries for resourcePack.name and resourcePack.description
    - Updates the client initializer to register the pack

--- src/client/java/de/pnku/more_nemos_woodcutter_variants/MoreNemosWoodcutterVariantsClient.java
+++ src/client/java/de/pnku/more_nemos_woodcutter_variants/MoreNemosWoodcutterVariantsClient.java
@@ -4,7 +4,12 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.renderer.RenderType;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.world.level.block.Block;
 
 import static de.pnku.more_nemos_woodcutter_variants.block.MoreWoodcutterBlocks.more_woodcutter_blocks;
@@ -17,5 +22,11 @@ public class MoreNemosWoodcutterVariantsClient implements ClientModInitializer {
         for (Block woodcutterBlock : more_woodcutter_blocks) {
             BlockRenderLayerMap.INSTANCE.putBlock(woodcutterBlock, RenderType.cutout());
         }
+        ResourceManagerHelper.registerBuiltinResourcePack(
+                MoreNemosWoodcutterVariants.withModId(MoreNemosWoodcutterVariants.MOD_ID),
+                FabricLoader.getInstance().getModContainer(MoreNemosWoodcutterVariants.MOD_ID).orElseThrow(),
+                Component.translatable("resourcePack." + MoreNemosWoodcutterVariants.MOD_ID + ".name"),
+                ResourcePackActivationType.ALWAYS_ENABLED
+        );
     }
 }
